8564cf3fc5e0c52879753fde6919a78b60f130c7
54LY <113955564+54LY@users.noreply.github.com>
2025-11-14 11:33:48 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 // 为首页的“了解更多”按钮添加点击事件
-document.getElementById("learnMore")?.addEventListener("click", function() {
-    alert("感谢你的关注！这是一个简单的交互示例。");
-});
+document.getElementById("learnMore")
+	?.addEventListener("click", function()
+	{
+		alert("感谢你的关注！这是一个简单的交互示例。");
+	}
+					);
+					
+document.getElementById("learnMore2")
+	?.addEventListener("click", function()
+	{
+		alert("");
+	}
+					);
+					
